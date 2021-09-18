49d540de7948831c4155d4a127a8fa6fb1dcfd7e
Alexander Piskun <bigcat88@icloud.com>
2021-09-18 16:28:51 +0300

    actions...

--- lib/Service/PythonService.php
+++ lib/Service/PythonService.php
@@ -23,7 +23,7 @@ declare(strict_types=1);
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ *!
  */
 
 namespace OCA\MediaDC\Service;
