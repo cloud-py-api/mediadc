80ed96b047fdcfc52312b620dcdb68112e08378d
Andrey Borysenko <andrey18106x@gmail.com>
2021-09-19 20:01:22 +0300

    Tests boilerplate

---
+++ tests/bootstrap.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @copyright Copyright (c) 2021 Andrey Borysenko <andrey18106x@gmail.com>
+ * 
+ * @copyright Copyright (c) 2021 Alexander Piskun <bigcat88@icloud.com>
+ * 
+ * @author 2021 Andrey Borysenko <andrey18106x@gmail.com>
+ *
+ * @license AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+define('PHPUNIT_RUN', 1);
+
+require_once __DIR__.'/../../../lib/base.php';
+require_once __DIR__.'/../vendor/autoload.php';
+
+\OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
+\OC_App::loadApp('mediadc');
+
+OC_Hook::clear();
